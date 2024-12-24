50d12024d0fdcecb6c4f44cb1c06b00156195100
dhruvilrathod <dhruvilrathod2001@gmail.com>
2024-12-24 20:54:57 +1030

    Advent of Code Day 24 - part 2

--- advent_of_code/day24.py
+++ advent_of_code/day24.py
@@ -444,4 +444,143 @@ ans1, solved_wiremap, operations_map = calculate_output(text)
 print("answer 1: ", ans1)
 
 
+def parse_input_part2(text):
+    initials, operations = text.split("\n\n")
+    initial_values = {}
+    gate_map = {}
 
+    for line in initials.split("\n"):
+        key, value = line.split(": ")
+        initial_values[key] = int(value)
+
+    for line in operations.split("\n"):
+        parts = line.split(" ")
+        input1, operator, input2, _, output = parts
+        gate_map[output] = (operator, input1, input2, output)
+
+    return initial_values, gate_map
+
+def evaluate_gate(gate, gate_map, initial_values):
+    operator, input1, input2, _ = gate
+
+    if input1 in initial_values:
+        input1_value = initial_values[input1]
+    else:
+        input1_value = evaluate_gate(gate_map[input1], gate_map, initial_values)
+
+    if input2 in initial_values:
+        input2_value = initial_values[input2]
+    else:
+        input2_value = evaluate_gate(gate_map[input2], gate_map, initial_values)
+
+    if operator == "AND":
+        return input1_value & input2_value
+    elif operator == "OR":
+        return input1_value | input2_value
+    elif operator == "XOR":
+        return input1_value ^ input2_value
+
+def evaluate_system(gate_map, initial_values):
+    outputs = []
+    terminal_gates = [(out, gate) for out, gate in gate_map.items() if out.startswith('z')]
+    for out, terminal_gate in terminal_gates:
+        p = int(out[1:])
+        outputs.append((p, evaluate_gate(terminal_gate, gate_map, initial_values)))
+
+    return sum([2**p * value for p, value in outputs])
+
+def check_op_map(op_map, x_var, y_var, op):
+    if (op, x_var, y_var) in op_map:
+        return op_map[(op, x_var, y_var)]
+    elif (op, y_var, x_var) in op_map:
+        return op_map[(op, y_var, x_var)]
+    return ""
+
+def carry_forward_check(op_map, gate_map, num_outputs):
+    carry_forward = ""
+    new_carry = ""
+    to_be_verified_list = []
+    checked_list = []
+
+    for i in range(num_outputs):
+        index = f"%02d" % i
+        x_var = f"x{index}"
+        y_var = f"y{index}"
+        z_var = f"z{index}"
+
+        digit1 = check_op_map(op_map, x_var, y_var, "XOR")
+        carry1 = check_op_map(op_map, x_var, y_var, "AND")
+
+        if i == 0:
+            if digit1 != z_var:
+                return 0, checked_list
+            checked_list.append(digit1)
+            carry_forward = carry1
+        else:
+            digit2 = check_op_map(op_map, carry_forward, digit1, "XOR")
+
+            if digit2 != z_var:
+                return i - 1, checked_list
+
+            checked_list.append(digit1)
+            for item in to_be_verified_list:
+                checked_list.append(item)
+
+            new_carry = check_op_map(op_map, carry_forward, digit1, "AND")
+            carry_forward = check_op_map(op_map, carry1, new_carry, "OR")
+
+            to_be_verified_list.append(carry_forward)
+            to_be_verified_list.append(new_carry)
+
+    return num_outputs, checked_list
+
+def find_swaps(gate_map):
+    op_map = {}
+    for out, gate in gate_map.items():
+        op_map[(gate[0], gate[1], gate[2])] = out
+
+    terminal_gates = [(out, gate) for out, gate in gate_map.items() if out.startswith('z')]
+    num_outputs = len(terminal_gates)
+
+    swaps = set()
+    personal_best, checked_list = carry_forward_check(op_map, gate_map, num_outputs)
+
+    for _ in range(4):
+        for op1, op2 in combinations(gate_map.keys(), 2):
+            gate1 = gate_map[op1]
+            gate2 = gate_map[op2]
+
+            if op1 in checked_list or op2 in checked_list:
+                continue
+
+            op_map[(gate1[0], gate1[1], gate1[2])] = op2
+            op_map[(gate2[0], gate2[1], gate2[2])] = op1
+
+            gate_map[op1] = gate2
+            gate_map[op2] = gate1
+
+            new_attempt, checked_in_attempt = carry_forward_check(op_map, gate_map, num_outputs)
+
+            if new_attempt > personal_best:
+                personal_best = new_attempt
+                checked_list = checked_in_attempt
+
+                swaps.add((op1, op2))
+                break
+
+            op_map[(gate1[0], gate1[1], gate1[2])] = op1
+            op_map[(gate2[0], gate2[1], gate2[2])] = op2
+
+            gate_map[op1] = gate1
+            gate_map[op2] = gate2
+
+    swaps_sorted = sorted(sum(swaps, start=tuple()))
+    return ",".join(swaps_sorted)
+
+def find_all_swaps(text):
+    initial_values, gate_map = parse_input_part2(text)
+    swaps = find_swaps(gate_map)
+    return swaps
+
+
+print("asnwer 2:", find_all_swaps(text))
